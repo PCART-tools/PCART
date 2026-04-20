7313359f2192b383710adf68c37c679afb596f68
Guanping Xiao <xiaogp13@163.com>
2026-04-20 20:05:12 +0800

    Fix preprocess runPath segment counting
    Normalize trailing separators before counting runPath directory levels in preprocess instrumentation paths.

--- Preprocess/preprocess.py
+++ Preprocess/preprocess.py
@@ -645,8 +645,8 @@ def addDictAll(projPath,projName,filePath,runFileLst,libName,runPath,runCommand)
         #当runPath不在runCommand中时，需要切换到运行文件所在的目录执行命令
         #而文件操作的相对路径就是相对于命令执行的路径
         if runPath!='' and runPath not in runCommand:
-            normalized_runPath = runPath.replace('\\', '/')
-            l=len(normalized_runPath.split('/'))
+            normalized_runPath = runPath.replace('\\', '/').strip('/')
+            l=len([segment for segment in normalized_runPath.split('/') if segment])
             while l>0:
                 pklPrefix='../'+pklPrefix
                 l-=1
@@ -781,8 +781,8 @@ def handleRunFile(file,runPath,runCommand):
     #而文件操作的相对路径就是相对于命令执行的路径
     pklPrefix=f"../../Copy/pkl"
     if runPath!='' and runPath not in runCommand:
-        normalized_runPath = runPath.replace('\\', '/')
-        l=len(normalized_runPath.split('/'))
+        normalized_runPath = runPath.replace('\\', '/').strip('/')
+        l=len([segment for segment in normalized_runPath.split('/') if segment])
         while l>0:
             pklPrefix='../'+pklPrefix
             l-=1
